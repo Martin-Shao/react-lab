00cf734b6dac50264c68877a38707ad704d65a06
Martin <shaogucheng@hotmail.com>
2020-06-07 19:24:26 +0800

    update: App组件更新

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,52 @@
-import React from 'react';
-import CardWrapper from './layout/CardWrapper'
+import React, { useContext, createContext } from 'react';
+// import CardWrapper from './layout/CardWrapper'
 
-class App extends React.Component {
-  render() {
-    return (
-      <CardWrapper />
-    )
+const themes = {
+  light: {
+    foreground: "#000000",
+    background: "#eeeeee"
+  },
+  dark: {
+    foreground: "#ffffff",
+    background: "#222222"
   }
+};
+
+const ThemeContext = createContext(themes.light);
+
+function App() {
+  console.info(useContext(createContext()))
+  return (
+    <ThemeContext.Provider value={themes.dark}>
+      <Toolbar />
+    </ThemeContext.Provider>
+  );
 }
+
+function Toolbar(props) {
+  return (
+    <div>
+      <ThemedButton />
+    </div>
+  );
+}
+
+function ThemedButton() {
+  const theme = useContext(ThemeContext);
+  console.info(theme)
+  return (
+    <button style={{ background: theme.background, color: theme.foreground }}>
+      I am styled by theme context!
+    </button>
+  );
+}
+
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <CardWrapper />
+//     )
+//   }
+// }
 
 export default App;
